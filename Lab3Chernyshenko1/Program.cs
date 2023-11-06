09415a2b2e1325ea07fd7cbbe6ef274c42457b5b
5090969 <5090969@LAPTOP-NB62C61V>
2023-11-06 23:23:22 +0200

    Добавьте файлы проекта.

---
+++ Lab3Chernyshenko1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+double X; 
+int N;
+Console.Write("Введите вещественное число X: ");
+X = double.Parse(Console.ReadLine());
+Console.Write("Введите целое число N (> 0): ");
+N = int.Parse(Console.ReadLine());
+double result = 1.0; 
+for (int i = 1; i <= N; i++)
+{
+    result += Math.Pow(X, i) / i;
+}
+
+Console.WriteLine("Результат: " + result);
